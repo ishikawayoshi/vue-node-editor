c27855bba9b0a967857fa23e07af61814edada98
Aleksandr Shpak <shpak@netcracker.com>
2019-10-01 15:11:30 +0400

    update dependencies, axios,buefy, vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import axios from "axios";
+
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+
+Vue.use(Buefy);
+
+Vue.prototype.axios = axios.create({
+  baseURL
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
